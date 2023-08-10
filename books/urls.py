f6256209f12f9dbb3af01ee2b1666189bf6d5b08
Sanjar1996 <xabibovsanjarbek@gmail.com>
2023-08-10 14:24:58 +0500

    serializer changed

--- books/urls.py
+++ books/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
 
-from .views import BookListApiView, BookDetailView, BookUpdateView, BookDeleteView, BookCreateView, \
-    Book, BookDeleteUpdateView, BookAPIView, BookCreate
+from .views import BookListApiView, BookDetailAPIView
+
+# BookListApiView, BookDetailView, BookUpdateView, BookDeleteView, BookCreateView, \
+# BookAPIView, BookCreate
+
+
+# urlpatterns = [
+#     path('', BookListApiView.as_view()),
+#     path('<int:pk>/', BookDetailView.as_view()),
+#     path('<int:pk>/update/', BookUpdateView.as_view()),
+#     path('<int:pk>/delete/', BookDeleteView.as_view()),
+#     path('create/', BookCreateView.as_view()),
+#     path('apiview/', BookAPIView.as_view()),
+#     path('create2/', BookCreate.as_view())
+# ]
+
 
 urlpatterns = [
-    path('', BookListApiView.as_view()),
-    path('<int:pk>/', BookDetailView.as_view()),
-    path('<int:pk>/update/', BookUpdateView.as_view()),
-    path('<int:pk>/delete/', BookDeleteView.as_view()),
-    path('create/', BookCreateView.as_view()),
-    path('apiview/', BookAPIView.as_view()),
-    path('create2/', BookCreate.as_view())
+    path('book/', BookListApiView.as_view()),
+    path('book/<int:pk>/', BookDetailAPIView.as_view()),
 ]
--- books/views.py
+++ books/views.py
@@ -1,79 +1,106 @@
+# from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import BookModel
 from .serializers import BookSerializers
-from rest_framework.views import APIView
-from rest_framework import generics
-from rest_framework.response import Response
 
-# from drf_yasg.openapi import Response
 book_model = BookModel.objects.all()
 
 
-class BookListApiView(generics.ListAPIView):
-    queryset = book_model
-    serializer_class = BookSerializers
+#
+# class BookListApiView(generics.ListAPIView):
+#     queryset = book_model
+#     serializer_class = BookSerializers
+#
+#
+# class BookAPIView(APIView):
+#
+#     def get(self, request):
+#         book = BookModel.objects.all()
+#         print(book)
+#         serializer_data = BookSerializers(book, many=True).data
+#         data = {
+#             'status': f"{len(book)} dona kitob",
+#             'books': serializer_data,
+#         }
+#
+#         return Response(data)
+#
+#
+# class BookCreate(APIView):
+#
+#     def post(self, request):
+#         data = request.data
+#         serializers = BookSerializers(data=data)
+#
+#         if serializers.is_valid():
+#             book1 = serializers.save()
+#             data = {
+#                 'status': 'OK',
+#                 'books': data
+#             }
+#             return Response(data)
+#
+#         return Response(data)
+#
+#
+# class BookDetail(APIView):
+#
+#     def get(self, request, pk):
+#         book = BookModel.objects.get(id=pk)
+#         serializer = BookSerializers(book).data
+#
+#
+# class BookDetailView(generics.RetrieveAPIView):
+#     queryset = book_model
+#     serializer_class = BookSerializers
+#
+#
+# class BookDeleteView(generics.DestroyAPIView):
+#     queryset = book_model
+#     serializer_class = BookSerializers
+#
+#
+# class BookUpdateView(generics.UpdateAPIView):
+#     queryset = book_model
+#     serializer_class = BookSerializers
+#
+#
+# class BookCreateView(generics.CreateAPIView):
+#     queryset = book_model
+#     serializer_class = BookSerializers
+#
+#
+# class Book(generics.ListCreateAPIView):
+#     queryset = book_model
+#     serializer_class = BookSerializers
+#
+#
+# class BookDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = book_model
+#     serializer_class = BookSerializers
+#
+#
+# class BookApiView(APIView):
+#
+#     def get(self, request):
+#         book = BookModel.objects.all()
 
-
-class BookAPIView(APIView):
-
+class BookListApiView(APIView):
     def get(self, request):
         book = BookModel.objects.all()
-        print(book)
-        serializer_data = BookSerializers(book, many=True).data
+        serializers_book = BookSerializers(book, many=True).data
         data = {
-            'status': f"{len(book)} dona kitob",
-            'books': serializer_data,
+            'status': f"{len(book)} ta kitob bor",
+            'serializer': serializers_book
         }
 
         return Response(data)
 
 
-class BookCreate(APIView):
+class BookDetailAPIView(APIView):
+    def post(self, request, pk):
+        book = BookModel.objects.get(id=pk)
 
-    def post(self, request):
-        data = request.data
-        serializers = BookSerializers(data=data)
-
-        if serializers.is_valid():
-            book1 = serializers.save()
-            data = {
-                'status': 'OK',
-                'books': data
-            }
-            return Response(data)
-
-        return
-
-class BookDetailView(generics.RetrieveAPIView):
-    queryset = book_model
-    serializer_class = BookSerializers
-
-
-class BookDeleteView(generics.DestroyAPIView):
-    queryset = book_model
-    serializer_class = BookSerializers
-
-
-class BookUpdateView(generics.UpdateAPIView):
-    queryset = book_model
-    serializer_class = BookSerializers
-
-
-class BookCreateView(generics.CreateAPIView):
-    queryset = book_model
-    serializer_class = BookSerializers
-
-
-class Book(generics.ListCreateAPIView):
-    queryset = book_model
-    serializer_class = BookSerializers
-
-
-class BookDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = book_model
-    serializer_class = BookSerializers
-
-
-class BookApiView(APIView):
-
-    def get(self, request):
-        book = BookModel.objects.all()
+        serializers = BookSerializers(book).data
